489ad7afb4dbb1a1484103002c3388587b56f6fc
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-23 16:57:11 +0100

    Update __init__.py

--- components/st3215_servo/__init__.py
+++ components/st3215_servo/__init__.py
@@ -3,10 +3,15 @@ from esphome.components import uart
 
 st3215_ns = cg.esphome_ns.namespace("st3215_servo")
 
-# TADY JE KLÍČOVÉ PŘIDAT uart.UARTDevice !!!
 St3215Servo = st3215_ns.class_(
     "St3215Servo",
     cg.PollingComponent,
     uart.UARTDevice,
-    cg.Component
+    cg.Component,
 )
+
+# ESPHome bere CONFIG_SCHEMA pro top-level platformu z __init__.py,
+# tak ho sem jen re-exportujeme.
+from .st3215_servo import CONFIG_SCHEMA, to_code  # noqa
+
+DEPENDENCIES = ["uart"]
