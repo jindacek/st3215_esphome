7c0754cacb43f2b704db06388dc9e22de35bc7f6
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-23 16:06:02 +0100

    Update __init__.py

--- components/st3215_servo/__init__.py
+++ components/st3215_servo/__init__.py
@@ -1,4 +1,12 @@
 import esphome.codegen as cg
+from esphome.components import uart
 
 st3215_ns = cg.esphome_ns.namespace("st3215_servo")
-St3215Servo = st3215_ns.class_("St3215Servo", cg.PollingComponent, cg.Component)
+
+# TADY JE KLÍČOVÉ PŘIDAT uart.UARTDevice !!!
+St3215Servo = st3215_ns.class_(
+    "St3215Servo",
+    cg.PollingComponent,
+    uart.UARTDevice,     # <- MUSÍ TAM BÝT
+    cg.Component
+)
