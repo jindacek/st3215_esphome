c0d4bab2b8af0b8012f3d1f3825d3af6a84654a1
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-23 17:33:54 +0100

    Update __init__.py

--- components/st3215_servo/__init__.py
+++ components/st3215_servo/__init__.py
@@ -1,6 +1,9 @@
 import esphome.codegen as cg
 from esphome.components import uart
 
+AUTO_LOAD = ["sensor"]          # <- Tohle vynutí přidání sensor komponenty do buildu
+DEPENDENCIES = ["uart"]         # <- UART je skutečná závislost
+
 st3215_ns = cg.esphome_ns.namespace("st3215_servo")
 
 St3215Servo = st3215_ns.class_(
@@ -10,8 +13,4 @@ St3215Servo = st3215_ns.class_(
     cg.Component,
 )
 
-# ESPHome bere CONFIG_SCHEMA pro top-level platformu z __init__.py,
-# tak ho sem jen re-exportujeme.
 from .st3215_servo import CONFIG_SCHEMA, to_code  # noqa
-
-DEPENDENCIES = ["uart"]
