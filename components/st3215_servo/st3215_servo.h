681cd7703b172305eb97d4e2b704ee82847c6d83
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-27 17:19:33 +0100

    Update st3215_servo.h

--- components/st3215_servo/st3215_servo.h
+++ components/st3215_servo/st3215_servo.h
@@ -27,6 +27,9 @@ class St3215TorqueSwitch : public switch_::Switch, public Component {
 // ===================== Servo =====================
 class St3215Servo : public PollingComponent, public uart::UARTDevice {
  public:
+  St3215Servo()
+      : PollingComponent(500), uart::UARTDevice(nullptr) {}
+
   St3215Servo(uart::UARTComponent *parent, uint8_t id)
       : PollingComponent(500), uart::UARTDevice(parent), servo_id_(id) {}
 
@@ -34,54 +37,80 @@ class St3215Servo : public PollingComponent, public uart::UARTDevice {
   void dump_config() override;
   void update() override;
 
-  // Pohyb
+  // povinné SETTERY (volá main.cpp)
+  void set_servo_id(uint8_t id) { servo_id_ = id; }
+  void set_max_angle(float a) { max_angle_ = a; }
+  void set_turns_full_open(float t) { turns_full_open_ = t; }
+
+  // pohyb
   void rotate(bool cw);
   void stop();
 
-  // Torque
+  // torque
   void set_torque(bool on);
   void set_torque_switch(St3215TorqueSwitch *s);
 
-  // Kalibrace
+  // kalibrace
   void set_zero();
   void set_max();
 
-  // Senzory
+  // senzory
   void set_angle_sensor(sensor::Sensor *s) { angle_sensor_ = s; }
   void set_turns_sensor(sensor::Sensor *s) { turns_sensor_ = s; }
   void set_percent_sensor(sensor::Sensor *s) { percent_sensor_ = s; }
 
  protected:
-  // Servo ID
   uint8_t servo_id_{1};
+  float max_angle_{240};
+  float turns_full_open_{0};
 
-  // Stav torque
   bool torque_on_{true};
 
-  // Soft multitur n
   uint16_t last_raw_{0};
   bool have_last_{false};
   float turns_unwrapped_{0};
 
-  // Kalibrace
   float zero_offset_{0};
   float max_turns_{0};
   bool has_zero_{false};
   bool has_max_{false};
 
-  // Senzory
   sensor::Sensor *angle_sensor_{nullptr};
   sensor::Sensor *turns_sensor_{nullptr};
   sensor::Sensor *percent_sensor_{nullptr};
 
-  // Switch
   St3215TorqueSwitch *torque_switch_{nullptr};
 
-  // UART helper
   uint8_t checksum_(const uint8_t *data, size_t len);
   void send_packet_(uint8_t id, uint8_t cmd, const std::vector<uint8_t> &params);
   bool read_registers_(uint8_t id, uint8_t addr, uint8_t len, std::vector<uint8_t> &out);
 };
+
+
+class St3215RotateAction : public Action<> {
+ public:
+  explicit St3215RotateAction(St3215Servo *p) : parent_(p) {}
+  void set_direction(bool cw) { cw_ = cw; }
+
+  void play(ActionContext &ctx) override {
+    parent_->rotate(cw_);
+  }
+
+ protected:
+  St3215Servo *parent_;
+  bool cw_{true};
+};
+
+class St3215StopAction : public Action<> {
+ public:
+  explicit St3215StopAction(St3215Servo *p) : parent_(p) {}
+
+  void play(ActionContext &ctx) override { parent_->stop(); }
+
+ protected:
+  St3215Servo *parent_;
+};
+
 
 }  // namespace st3215_servo
 }  // namespace esphome
