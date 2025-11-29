d47cf424602aa9f6258ba596f8a0b0ffd2f89519
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-29 14:06:40 +0100

    Update st3215_servo.h

--- components/st3215_servo/st3215_servo.h
+++ components/st3215_servo/st3215_servo.h
@@ -13,6 +13,18 @@ namespace st3215_servo {
 
 static const float RAW_PER_TURN = 4096.0f;
 
+// ======================== Torque Switch ========================
+class St3215Servo;
+
+class St3215TorqueSwitch : public switch_::Switch, public Component {
+ public:
+  void set_parent(St3215Servo *p) { parent_ = p; }
+  void write_state(bool state) override;
+ protected:
+  St3215Servo *parent_{nullptr};
+};
+
+// ====================== Kalibrační stav ========================
 enum CalibState {
   CALIB_IDLE = 0,
   CALIB_READY = 1,
@@ -21,73 +33,75 @@ enum CalibState {
   CALIB_ERROR = 4
 };
 
-class St3215TorqueSwitch : public Component, public switch_::Switch {
+// =========================== Servo =============================
+class St3215Servo : public PollingComponent, public uart::UARTDevice {
  public:
-  void set_parent(class St3215Servo *parent) { parent_ = parent; }
+  St3215Servo()
+    : PollingComponent(500), uart::UARTDevice(nullptr) {}
 
- protected:
-  void write_state(bool state) override;
+  St3215Servo(uart::UARTComponent *parent, uint8_t id)
+    : PollingComponent(500), uart::UARTDevice(parent), servo_id_(id) {}
 
-  class St3215Servo *parent_{nullptr};
-};
-
-class St3215Servo : public Component, public uart::UARTDevice {
- public:
   void setup() override;
-  void loop() override;
+  void dump_config() override;
+  void update() override;
 
+  // YAML settery
+  void set_servo_id(uint8_t id) { servo_id_ = id; }
+  void set_turns_full_open(float t) { max_turns_ = t; has_max_ = true; }
+  void set_max_angle(float) {}   // kompatibilita
+
+  // Ovládání
   void rotate(bool cw, int speed);
   void stop();
+  void set_torque(bool on);
 
+  // Kalibrace
   void set_zero();
   void set_max();
-
   void start_calibration();
   void confirm_calibration_step();
 
-  void set_servo_id(int id) { servo_id_ = id; }
-  void set_max_angle(float a) { max_angle_ = a; }
-  void set_turns_full_open(float t) { max_turns_ = t; }
-
+  // Sensory
   void set_angle_sensor(sensor::Sensor *s) { angle_sensor_ = s; }
   void set_turns_sensor(sensor::Sensor *s) { turns_sensor_ = s; }
   void set_percent_sensor(sensor::Sensor *s) { percent_sensor_ = s; }
-
   void set_calib_state_sensor(sensor::Sensor *s) { calib_state_sensor_ = s; }
 
-  void set_torque_switch(switch_::Switch *s) { torque_switch_ = s; }
+  // Switch
+  void set_torque_switch(St3215TorqueSwitch *s);
 
  protected:
-  void publish_angle_(float angle);
-  void publish_turns_(float turns);
-  void publish_percent_(float pct);
+  uint8_t servo_id_{1};
+  bool torque_on_{true};
 
-  void update_calib_state_(CalibState s);
+  uint16_t last_raw_{0};
+  bool have_last_{false};
+  float turns_unwrapped_{0};
 
-  int servo_id_{1};
-  float max_angle_{240.0f};
+  float zero_offset_{0};
   float max_turns_{0};
-
-  bool moving_{false};
-  bool direction_{true};
-  int speed_{600};
-
-  float current_turns_{0};
-  float zero_turns_{0};
-
   bool has_zero_{false};
   bool has_max_{false};
-  bool calibrated_{false};
 
+  // Kalibrace
   bool calibration_active_{false};
   CalibState calib_state_{CALIB_IDLE};
 
+  // Sensory
   sensor::Sensor *angle_sensor_{nullptr};
   sensor::Sensor *turns_sensor_{nullptr};
   sensor::Sensor *percent_sensor_{nullptr};
   sensor::Sensor *calib_state_sensor_{nullptr};
 
-  switch_::Switch *torque_switch_{nullptr};
+  St3215TorqueSwitch *torque_switch_{nullptr};
+
+  // Low level comm
+  uint8_t checksum_(const uint8_t *data, size_t len);
+  void send_packet_(uint8_t id, uint8_t cmd, const std::vector<uint8_t> &params);
+  bool read_registers_(uint8_t id, uint8_t addr, uint8_t len, std::vector<uint8_t> &out);
+
+  void update_calib_state_(CalibState s);
 };
 
 }  // namespace st3215_servo
