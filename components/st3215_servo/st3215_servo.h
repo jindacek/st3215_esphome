9f54f1d5818d9b8018f11f5df074fd4ce245d3fd
jindacek <105168152+jindacek@users.noreply.github.com>
2026-01-09 17:52:26 +0100

    Update st3215_servo.h

--- components/st3215_servo/st3215_servo.h
+++ components/st3215_servo/st3215_servo.h
@@ -6,6 +6,7 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/number/number.h"
 #include "esphome/core/helpers.h"
+#include "esphome/components/cover/cover.h"
 #include <vector>
 
 namespace esphome {
